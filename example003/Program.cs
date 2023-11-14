c548aeae21e7319e853945147e935f226a726fd5
Dolbnia Anastasia <nastya.1998sos@mail.ru>
2023-11-14 21:33:34 +0300

    рандом

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA =3;
-int NumberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 int result = numberA + NumberB;
 Console.WriteLine (result);
